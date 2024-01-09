cab3d89e4da8ecc2a945b38e3e5ea25876977019
VM <vlad.makarin22@gmail.com>
2024-01-09 10:04:51 +0300

    initial commit

---
+++ ExpertEngine.py
@@ -0,0 +1,55 @@
+from main import load_json, json_update
+
+
+class ExpertEngine:
+    def __init__(self, gifts, rules):
+        self._UserAnswers = None
+        self._gifts = gifts
+        self._rules = rules
+
+    def ask_user(self) -> None:
+        self._UserAnswers = dict()
+        for rule in self._rules["rules"]:
+            ans = input(f'{rule["question"]}\n')
+            if ans in rule["values"]:
+                self._UserAnswers.update({rule["property"]: ans})
+
+    def load_ans_json(self, path):
+        self._UserAnswers = load_json(path)
+
+    def get_recommendations(self) -> list:
+        recommendations = []
+
+        if self._UserAnswers:
+            for gift in self._gifts["gifts"]:
+                accepted_rules = []
+                gifts_rules_ids = gift["rules_id"]
+
+                for rule in [rule for rule in self._rules["rules"] if rule["id"] in gifts_rules_ids]:
+                    gift_property = gift["properties"][rule["property"]]
+                    user_answer = self._UserAnswers["answers"][rule["property"]]
+                    accepted_rules.append(eval('gift_property == user_answer'))
+
+                if all(accepted_rules):
+                    recommendations.append(gift["name"])
+
+        return recommendations
+
+    def get_rules(self) -> None:
+        return self._rules["rules"]
+
+    def remove_rule(self, id_to_remove) -> bool:
+        new_rules_list = [rule for rule in self._rules["rules"] if rule["id"] != id_to_remove]
+        self._rules["rules"] = new_rules_list
+        return json_update('rules.json', self._rules)
+
+    def add_rule(self, id: int, question: str, property_name: str, values: list) -> bool:
+        new_rule = {
+            "id": id,
+            "property": property_name,
+            "values": values,
+            "question": question
+        }
+
+        self._rules["rules"].append(new_rule)
+        return json_update('rules.json', self._rules)
---
+++ menu.py
@@ -0,0 +1,41 @@
+import os
+
+import keyboard
+
+
+class Menu:
+    def __init__(self, menu_items: dict = None, parent_menu=None):
+        self.menu_items = menu_items
+        self.parent_menu = parent_menu
+
+    def display_menu(self):
+        for idx, (item_name, _) in enumerate(zip(self.menu_items.keys(), self.menu_items.values()), start=1):
+            print(f'{idx} - {item_name}')
+        print('Нажмите ESC, чтобы выйти или вернуться на пункт выше.')
+        print('Выберите пункт меню...')
+
+    def run_menu(self):
+        self.display_menu()
+        while True:
+            key_event = keyboard.read_event(suppress=True)
+            if key_event.event_type == keyboard.KEY_DOWN:
+                if key_event.name.isdigit():
+                    choice = int(key_event.name)
+                    if 0 <= choice <= len(self.menu_items):
+                        if choice == 0 and self.parent_menu is not None:
+                            return
+                        selected_item = list(self.menu_items.keys())[choice - 1]
+                        action = self.menu_items[selected_item]
+                        if callable(action):
+                            action()
+                            self.run_menu()
+                        elif isinstance(action, Menu):
+                            action.parent_menu = self
+                            action.run_menu()
+                    else:
+                        print("Неверный выбор. Попробуйте еще раз.")
+            elif key_event.name == 'esc':
+                if self.parent_menu is not None:
+                    self.parent_menu.run_menu()
+                else:
+                    exit()
